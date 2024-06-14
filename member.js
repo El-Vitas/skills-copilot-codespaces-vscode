680321e67ede0bc0b92543abde6f5ba104dba177
Jonathan Olivares Salinas <74159442+El-Vitas@users.noreply.github.com>
2024-06-14 01:40:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember()
+{
+    var member = {
+        name: "John",
+        age: 23,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 3500
+    };
+
+    return member;
+}
